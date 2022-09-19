58e228c1b43a655e24dc9123fcec1c1c6046c3ac
obinna aguwa <macmartins081@gmail.com>
2022-09-19 11:09:25 +0100

    installed stack and dependencies

---
+++ lib/app/router.dart
@@ -0,0 +1,10 @@
+import 'package:auto_route/annotations.dart';
+import 'package:stack_app/ui/views/home/home_view.dart';
+import 'package:stack_app/ui/views/startup/startup_view.dart';
+
+@MaterialAutoRouter()
+class $Router {
+  // @initial
+  // StartupView startupViewRoute;
+  // HomeView homeViewRoute;
+}
---
+++ lib/ui/views/startup/startup_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'startup_viewmodel.dart';
+
+class StartupView extends StatelessWidget {
+  const StartupView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<StartupViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text(model.title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: model.updateCounter,
+        ),
+      ),
+      viewModelBuilder: () => StartupViewModel(),
+    );
+  }
+}
